1f4b00e74dfc71727318778d2d426debf963b777
Felipe Luna <fsl@cesar.org.br>
2016-12-20 15:09:30 -0300

    in progress

---
+++ AoC2016/src/Day3/Solver.java
@@ -0,0 +1,32 @@
+package Day3;
+
+import java.io.*;
+
+/**
+ * Created by felipeluna on 20/12/16.
+ */
+public class Solver {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(new File("src/Day3/input.txt")));
+
+        String line;
+        while((line = br.readLine()) != null){
+            System.out.println(line);
+            
+//            String[] sides = line.split(" ");
+//            int s1 = Integer.parseInt(sides[0]);
+//            int s2 = Integer.parseInt(sides[1]);
+//            int s3 = Integer.parseInt(sides[2]);
+//            System.out.println(s1);
+//            System.out.printf("{%d, %d}", s1, s2);
+//            int counter = 0;
+//            if(((s1 + s2) > s3) && ((s2 + s3) > s1) && ((s1 + s3) > s2)){
+//                counter++;
+//            }
+
+
+        }
+
+    }
+
+}
