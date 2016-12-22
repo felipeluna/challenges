de320d4e67b2033ff2d2b5b8b2ff589160856e40
Felipe Luna <fsl@cesar.org.br>
2016-12-22 13:30:25 -0300

    Starting Day 5

---
+++ AoC2016/src/Day7/Palindrome.java
@@ -0,0 +1,17 @@
+package Day7;
+
+/**
+ * Created by felipeluna on 22/12/16.
+ * Aoc 2016 Day 7
+ */
+class Palindrome {
+    private String code;
+
+    Palindrome(String code){
+        this.code = code;
+    }
+
+    boolean isPalindrome(){
+        return false;
+    }
+}
---
+++ AoC2016/src/Day7/Solver.java
@@ -0,0 +1,8 @@
+package Day7;
+
+/**
+ * Created by felipeluna on 22/12/16.
+ * AoC 2016 Day 7
+ */
+public class Solver {
+}
