47d0357b84fa040f45c4c015059cd9ebd265866f
felipe <felipe@luna.com>
2016-12-24 17:32:01 -0300

    day 7 part one done.

--- AoC2016/src/Day7/Solver.java
+++ AoC2016/src/Day7/Solver.java
@@ -1,6 +1,9 @@
 package Day7;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,33 +12,49 @@ import java.util.regex.Pattern;
  * AoC 2016 Day 7
  */
 public class Solver {
+
+
+    private static boolean isTsl(String s){
+        Pattern p = Pattern.compile("(.)((?!\\1).)\\2\\1");
+        Matcher m = p.matcher(s);
+
+        while(m.find()){
+            return true;
+        }
+        return false;
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(new File("src/Day7/input.txt")));
         String line;
-        String input = "abba[mnop]qrst";
-        Matcher m = Pattern.compile("\\[[^\\]]*\\]|(\\b\\w+\\b)").matcher(input);
-        int counter = 0;
-        while(m.find()){
-            if(m.group().startsWith("[")){
-                String middle = m.group().substring(1, m.group().length()-1);
-                 // if middle is palindrome
-                char[] middleChar = middle.toCharArray();
-                for (int i = 0; i < middleChar.length/2; i++) {
-                    if(middleChar[i] != middleChar[middleChar.length - 1 - i]){
+        int generalCounter = 0;
+
+        while ((line = br.readLine()) != null) {
+            int noBrackets = 0;
+            int brackets = 0;
+            Matcher m = Pattern.compile("\\[[^\\]]*\\]|(\\b\\w+\\b)").matcher(line);
+            while (m.find()) {
+                if (m.group().startsWith("[")) {
+                    String middle = m.group().substring(1, m.group().length() - 1);
+                    // if middle is palindrome
+                    if (isTsl(middle)) {
+                        brackets++;
                         break;
                     }
+
+                } else {
+                    String middle = m.group();
+                    if (isTsl(middle)) {
+                        noBrackets++;
+                    }
+
                 }
-            }else{
-                String middle = m.group().substring(1, m.group().length()-1);
-                // if middle is palindrome
-                char[] middleChar = middle.toCharArray();
-                for (int i = 0; i < middleChar.length/2; i++) {
-                    if(middleChar[i] != middleChar[middleChar.length - 1 - i]){
-                        counter++;
-                        break;
-                    }
+            }
+            if (brackets == 0) {
+                if (noBrackets > 0) {
+                    generalCounter++;
                 }
             }
         }
+        System.out.println(generalCounter);
     }
 }
